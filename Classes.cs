873679b8ee85cb48434f840233deee275df067d6
Grozdan Andonov <grozdan.andonov@softwaregroup.com>
2023-05-04 15:18:38 +0300

    initial version

---
+++ Classes.cs
@@ -0,0 +1,42 @@
+// View Model
+class MainWindowVM {
+    private Article article;
+    public Article DisplayArticle
+    {
+        get { return article; }
+        set { article = value; }
+    }
+
+    public string FileAddr { get; set; }
+    public ICommand AddCommand { get; set; }
+
+    public MainWindowVM() {
+        AddCommand = new RelayCommand(getFileAndSend);
+    }
+    private void getFileAndSend(object obj) {
+        // има имплементация на getFileAndSend...
+    }
+}
+
+ // има имплементация на RelayCommand...
+
+ // Model
+ public class ArticlesContext : DbContext {
+    
+    public ArticleContext() : base() { }
+    public DbSet<Article> Articles { get; set; }
+
+    class Article {
+        public int ArticleId { get; set; }
+        public string Title { get; set; }
+        public string Autors { get; set; }
+        public string KeyWords { get; set; }
+        public byte[] PdfFile { get; set; }
+
+        public string GetStringFromPdfFile() {
+            // има имплементация на getStringFromPdfFile
+        }
+
+    }
+
+ }
